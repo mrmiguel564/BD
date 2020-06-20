94db242420aad4583c1e507a9bc51d44a6a25dd1
mrmiguel564 <miguel.espinoza@alumnos.uv.cl>
2020-06-20 16:32:03 -0400

    comienza la vida

---
+++ Script.py
@@ -0,0 +1,30 @@
+import mysql.connector
+
+#modificar esto segun tus datos
+Passwd="M564231897"
+Ip="localhost"
+User="root"
+
+mydb = mysql.connector.connect(host=Ip,user=User,password=Passwd)
+mycursor = mydb.cursor()
+
+#---------------------------------------------
+NombreBD="seguros_vida"
+try:
+	mycursor.execute("CREATE DATABASE "+NombreBD+"; use "+NombreBD)
+except:
+	mydb = mysql.connector.connect(host=Ip,user=User,password=Passwd,database=NombreBD)
+	mycursor = mydb.cursor()
+	print("BD "+NombreBD+" ya estaba Creada")
+else:
+	print("BD "+NombreBD+" Creada")
+#---------------------------------------------
+tableName="Persona" 		
+
+try:										 	#columnas para modificar mysql lenguaje https://www.w3schools.com/python/python_mysql_create_table.asp
+	mycursor.execute("CREATE TABLE"+tableName+" (rut  VARCHAR(50) Primary KEY, name VARCHAR(50))")
+except:
+	print("tabla "+tableName+" ya estaba Creada")
+else:
+	print("tabla "+tableName+" Creada")
+#---------------------------------------------
