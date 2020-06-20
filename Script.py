6e07993e6d85d836dd27c7dbc57a9efcce6d5a3f
MiguelYanez <miguel.yanez@alumnos.uv.cl>
2020-06-20 17:46:31 -0400

    +Persona.atributos

--- Script.py
+++ Script.py
@@ -22,7 +22,7 @@ else:
 tableName="Persona" 		
 
 try:										 	#columnas para modificar mysql lenguaje https://www.w3schools.com/python/python_mysql_create_table.asp
-	mycursor.execute("CREATE TABLE"+tableName+" (rut  VARCHAR(50) Primary KEY, name VARCHAR(50))")
+	mycursor.execute("CREATE TABLE"+tableName+" (rut  VARCHAR(20) Primary KEY, nombre VARCHAR(30), estado_civil VARCHAR(30), fecha_nacimiento VARCHAR(30), telefono VARCHAR(30), correo VARCHAR(30), genero VARCHAR(30), enfermedades_cronicas VARCHAR(30))")
 except:
 	print("tabla "+tableName+" ya estaba Creada")
 else:
