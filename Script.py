c3fa7cdfc84230ee9dd65688b595ca4e30008635
mrmiguel564 <miguel.espinoza@alumnos.uv.cl>
2020-07-08 22:08:15 -0400

    ahhh

--- Script.py
+++ Script.py
@@ -1,5 +1,5 @@
 import mysql.connector
-
+import json
 #modificar esto segun tus datos
 Passwd="M564231897"
 Ip="localhost"
@@ -10,55 +10,27 @@ mycursor = mydb.cursor()
 #---------------------------------------------
 NombreBD="seguros_vida"
 try:
-	mycursor.execute("create database "+NombreBD+";")
-	mycursor.execute("use "+NombreBD)
-	print("BD "+NombreBD+" Creada")
-except:
-	mydb.close()
 	mydb = mysql.connector.connect(host=Ip,user=User,password=Passwd,database=NombreBD)
 	mycursor = mydb.cursor()
-	print("BD "+NombreBD+" ya estaba Creada")
+	print(" Conexion extablecida con: "+NombreBD)
+except:
+	print(" No se pudo establecer la conexion")
 #---------------------------------------------
-tablas=(
-	(('persona',		)	,"rut  VARCHAR(20) PRIMARY KEY, nombre VARCHAR(30), estado_civil VARCHAR(30) , fecha_nacimiento VARCHAR(30) , genero VARCHAR(30))"),
-	(('cliente',		)	,""),
-	(('trabajador',		)	,""),
-	(('surcursal',		)	,""),
-	(('asignacion',		)	,""),
-	(('beneficiario',	)	,""),
-	(('asegurado',		)	,""),
-	(('correo',			)	,""),
-	(('poliza',			)	,""),
-	(('cobros',			)	,""),
-	(('de_Muerte,',		)	,""),
-	(('de_Vida_Entera',	)	,""),
-	(('de_vida_flexible',)	,""),
-	(('de_supervivencia',)	,""),
-	(('de_invalidez',	)	,"")
-	)
-variables=[]
-for x in range(len(tablas)):
-	variables.append(True)
 
-mycursor.execute("SHOW TABLES")
-for x in mycursor:
-	i=0
-	for y in tablas:
-		if x ==y[0]:
-			variables[i]=False
-		i=i+1
-i=0
+with open("C:/Users/mr_miguel/Documents/BD/datos.json") as file:
+    data = json.load(file)
 
-for y in variables:
-	if(y):
-		tableName=tablas[i][0]
-		try:										 	
-			mycursor.execute("CREATE TABLE "+tablas[i][0][0]+" ("+tablas[i][1]+")")
-		except:
-			print("ERROR tabla "+tablas[i][0][0]+" no se pudo Crear")
-	i=i+1
 
-"""
+
+for y in data:
+	print(y)
+	try:
+		mycursor.execute("insert into "+y )
+	except:
+		print("no se pudo insertar los datos")
+	
+
+
 sql = "INSERT INTO Persona (rut, nombre, estado_civil, fecha_nacimiento, genero) VALUES (%s, %s, %s, %s, %s)"
 val = [
   ('Peter', 'Lowstreet 4', 'Lowstreet 4', 'Lowstreet 4', 'Lowstreet 4'),
@@ -75,6 +47,7 @@ val = [
   ('Chuck', 'Main Road 989', 'Main Road 989', 'Main Road 989', 'Main Road 989'),
   ('Viola', 'Sideway 1633', 'Sideway 1633', 'Sideway 1633', 'Sideway 1633')
 ]
+
 mycursor.executemany(sql, val)
 
 mydb.commit()
